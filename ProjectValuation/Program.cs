e3b3f5603b2af3cb7dbf2fd4f81623e44321ca30
dalexandre6 <47304497+dalexandre6@users.noreply.github.com>
2020-12-25 20:27:07 -0600

    IRR was added to the calculations! Check the video here: https://github.com/dalexandre6/Projects

--- ProjectValuation/Program.cs
+++ ProjectValuation/Program.cs
@@ -10,15 +10,29 @@ namespace ProjectValuation
         static void Main(string[] args)
 
         {
-
+            //Rate and Investment:
             double wacc = 0.35f;
             double inv1 = 30000f;
             double inv2 = 20000f;
-
+            //NPV Project A:
             double[] projectA = new double[] { 25000f, 25000f, 30000f };
             double total1 = Financial.NPV(wacc, ref projectA);
             double NPVA = -inv1 + total1;
+            
+            //IRR ProjectB:
+            double[] projectIRRA = new double[] { -30000f, 25000f, 25000f, 30000f };
+            double irrA = Financial.IRR(ref projectIRRA);
+            Console.WriteLine("This is the IRR for project a : {0} ", irrA);
 
+            Console.WriteLine("******************************************************");
+            //IRR B:
+            double[] projectIRRB = new double[] { -20000F, 26000f, 28000f, 48000f };
+            double irrB = Financial.IRR(ref projectIRRB);
+            Console.WriteLine("This is the IRR for project a : {0} ", irrB);
+
+            Console.WriteLine("******************************************************");
+
+            //NPV Project B:
             double[] projectB = new double[] { 26000f, 28000f, 48000f };
             double total2 = Financial.NPV(wacc, ref projectB);
             double NPVB = -inv2 + total2;
